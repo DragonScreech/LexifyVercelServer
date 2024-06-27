b48cf72cc7ff5d8e9d7ce8487ffd9ee520534988
Dragon Screech <dragon.screech76@gmail.com>
2024-06-27 17:27:13 -0500

    changed video to use image urls instead of files

--- api/generate-video-no-subtitles.py
+++ api/generate-video-no-subtitles.py
@@ -2,8 +2,8 @@ from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 from moviepy.editor import ImageSequenceClip
 import os
-import io
 import tempfile
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -14,34 +14,44 @@ def genVideoNoSubtitles():
     duration = int(request.form['duration'])
     strNumImages = request.form['imageCount']
     numImages = int(strNumImages)
-
+    
+    # Get the image URLs from the request
+    image_urls = [request.form[f'image_url_{index}'] for index in range(numImages)]
+    
     # Paths for temporary storage
     temp_dir = tempfile.gettempdir()
     image_paths = [os.path.join(temp_dir, f'image_{index}_{uid}.png') for index in range(numImages)]
     print(image_paths)
-
-    # Save the uploaded image files
+    
+    # Download the images from the URLs
     for index in range(numImages):
-        if f'image_{index}' not in request.files:
-            return jsonify({"error": f"Image file image_{index} is required"}), 400
-        image_file = request.files[f'image_{index}']
-        image_file.save(image_paths[index])
-        print(f'image{index} saved!')
-
+        image_url = image_urls[index]
+        image_path = image_paths[index]
+        download_image(image_url, image_path)
+        print(f'image {index} downloaded!')
+    
     # Create video without audio
     temp_video_path = os.path.join(temp_dir, f'final_video_{uid}.mov')
     print(temp_video_path)
     create_video_without_audio(image_paths, numImages, duration, temp_video_path)
-
+    
     return send_file(temp_video_path, as_attachment=True, download_name='final_video.mov')
+
+def download_image(url, path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(path, 'wb') as file:
+            file.write(response.content)
+    else:
+        raise Exception(f"Failed to download image from {url}")
 
 def create_video_without_audio(image_paths, num_images, duration, output_path):
     # Set a fixed display duration for each image
-    image_display_duration = duration/num_images  # Display each image for 1 second
-
+    image_display_duration = duration / num_images
+    
     # Create ImageSequenceClip
     video_clip = ImageSequenceClip(image_paths, durations=[image_display_duration] * num_images)
-
+    
     # Write the final video to file
     video_clip.write_videofile(output_path, fps=24, codec='libx264', preset='ultrafast')
 
